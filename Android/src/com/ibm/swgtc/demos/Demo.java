78c506771fee76a4d0c6cea4f32fbb8760ae5e82
Bryce Curtis <curtis.bryce@gmail.com>
2010-09-14 17:07:13 -0500

    Initial check-in.

---
+++ Android/src/com/ibm/swgtc/demos/Demo.java
@@ -0,0 +1,24 @@
+
+package com.ibm.swgtc.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.phonegap.*;
+
+public class Demo extends DroidGap
+{
+
+    /**
+     * Constructor
+     */
+    public Demo() {
+        super();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.appView.clearCache(true);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
